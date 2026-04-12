952e4c3d74734e6c820749abc8ac331f90f1d9a8
Valentino <AL03077250@tecmilenio.mx>
2026-04-12 17:22:10 -0600

    Create ContactListView.swift

---
+++ 2026_Challenges/Retos/2026/2026-04-11_agenda-de-contactos-con-busqueda-y-favoritos/Views/ContactListView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct ContactListView: View {
+    @StateObject var viewModel = ContacsViewModel()
+    @State private var showFormSheet: Bool = false
+    var body: some View {
+        NavigationStack {
+            if viewModel.contacts.isEmpty {
+                EmptyView()
+            } else {
+                List {
+                    ForEach($viewModel.contacts){ $contact in
+                        ContactComponentView(contact: contact, isFavorite: $contact.isFavorite)
+                    }
+                }
+                .listStyle(.plain)
+                .navigationTitle("Contactos")
+                .toolbar {
+                    Button {
+                        self.showFormSheet.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .sheet(isPresented: self.$showFormSheet) {
+                        FormContactView(viewModel: self.viewModel)
+                    }
+                }
+            }
+        }
+    }
+}
